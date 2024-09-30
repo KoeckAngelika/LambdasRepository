a101f20c145f3a6f6c82068229633352c7990dce
unknown <angelika.lena.koeck@gmail.com>
2024-09-30 20:57:43 +0200

    project one ready the real one

--- LambdasUebung/src/Main.java
+++ LambdasUebung/src/Main.java
@@ -4,9 +4,30 @@ public class Main {
         
     public static void main(String[] args) {
 
-        //project1();
+        project1();
         //project2();
         //project3();
+    }
+
+    public static void project1()
+    {
+        HalloJavamitForEach halloJavamitForEach = new HalloJavamitForEach();
+        List<String> oldListTyp = new ArrayList<>();
+        oldListTyp.add("A");
+        oldListTyp.add("B");
+        oldListTyp.add("C");
+
+        for (int i = 0; i < oldListTyp.size(); i++) {
+            System.out.println(oldListTyp.get(i));
+        }
+
+        oldListTyp.forEach(System.out::println);
+
+        for (String string : oldListTyp) {
+            Consumer ask = text -> string + "?";
+            halloJavamitForEach.setConsumer(ask);
+            halloJavamitForEach.formatText(string);
+        }
     }
 
 
