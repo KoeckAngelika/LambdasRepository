c46d6f731266eda704f06fb3f7f1e2c5ae6320d7
unknown <angelika.lena.koeck@gmail.com>
2024-09-30 20:59:31 +0200

    project two, not correct

--- LambdasUebung/src/Main.java
+++ LambdasUebung/src/Main.java
@@ -30,6 +30,67 @@ public class Main {
         }
     }
 
+    public static void project2()
+    {
+        NumberTester numberTest = new NumberTester();
+        Map<Integer, List<String>> listMap = new HashMap<>(numberTest.getListMap());
+
+
+
+        for (List<String> value : listMap.values()) {
+            for (String string : value) {
+                String[] parts = string.split(" ");
+                int number1 = Integer.parseInt(parts[0]);
+                int number2 = Integer.parseInt(parts[1]);
+
+                if (number1 == 1) {
+                    System.out.println("Gerade / Ungerade");
+                    numberTest.setOddEvenTester(number2);
+                    int evenNumberCalculate = numberTest.even(number2);
+                    if (evenNumberCalculate == 0)
+                    {
+                        System.out.println(number2 + " ODD");
+                    }else
+                    {
+                        System.out.println(number2 + " EVEN");
+                    }
+                    System.out.println();
+
+                } else if (number1 == 2) {
+                    System.out.println("Primzahl / KeinePrimzahl");
+                    numberTest.setPrimeTester(number2);
+                    boolean primNumberCalculate = numberTest.prime(number2);
+                    if (primNumberCalculate)
+                    {
+                        System.out.println(number2 + " PRIME");
+                    }else
+                    {
+                        System.out.println(number2 + " NO PRIME");
+                    }
+                    System.out.println();
+
+
+                } else if (number1 == 3) {
+
+                    System.out.println("Palidrom");
+                    numberTest.setPalindromeTester(number2);
+                    int evenNumberCalculate = numberTest.palindrome(number2);
+                    if (evenNumberCalculate == 0)
+                    {
+                        System.out.println(number2 + " NO PALIDROM");
+                    }else
+                    {
+                        System.out.println(number2 + " PALIDROM");
+                    }
+                    System.out.println();
+                }
+
+            }
+
+        }
+    }
+
+
 
 
 
